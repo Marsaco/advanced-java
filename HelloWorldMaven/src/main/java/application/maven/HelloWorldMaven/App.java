42d03069d04229019ff65c7d2b5588db660c42e7
Marius <Marius@LAPTOP-82DMMFHK>
2021-06-26 16:19:48 +0100

    Maven push

---
+++ HelloWorldMaven/src/main/java/application/maven/HelloWorldMaven/App.java
@@ -0,0 +1,13 @@
+package application.maven.HelloWorldMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
